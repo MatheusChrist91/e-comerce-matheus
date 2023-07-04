42a023064e3bf18e848a53a983cc9beb2378455d
Matheus Christ <matheuschrist91@gmail.com>
2023-07-04 20:40:26 -0300

    fazer PR da maneira correta

--- src/hooks/useProductContext.js
+++ src/hooks/useProductContext.js
@@ -3,7 +3,7 @@ import { createContext, useEffect, useState } from "react";
 // PRODUTOS
 import db from "../db/db"
 
-// Varivel de crição do contexto
+// Varivel de crição do contextoO
 export const ProductContext = createContext();
 
 const ProductProvider = ({ children }) => {
